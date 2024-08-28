897d22556483c0a6defd85747213230a606b23ef
nabatt <naveen.battala@sas.com>
2024-08-28 00:35:42 -0400

    scr sidecar

--- post-deploy-config/source/config-sas-detection-scr-sidecar.sh
+++ post-deploy-config/source/config-sas-detection-scr-sidecar.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-oc patch deployment sas-detection --patch-file sas-detection-patch.yaml 
+oc patch deployment sas-detection --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/ports/0/containerPort", "value": 8777}]'
+oc patch deployment sas-detection --patch-file sas-detection-patch.yaml
+oc patch deployment sas-detection --patch-file scr-sidecar-patch.yaml
